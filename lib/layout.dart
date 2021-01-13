fe6c4bf2d5eefb9366087fc49b109e5fff515c54
2013612 <samuelwong2013612@gmail.com>
2021-01-13 22:54:01 +0800

    Add and Finish ryukyoku

--- lib/layout.dart
+++ lib/layout.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:jmpr_flutter/pointSetting.dart';
+import 'package:jmpr_flutter/ryukyoku.dart';
 import 'package:jmpr_flutter/setting.dart';
+import 'package:jmpr_flutter/common.dart';
+
 
 class Layout extends StatefulWidget {
   Layout({Key key, this.title}) : super(key: key);
@@ -11,18 +14,16 @@ class Layout extends StatefulWidget {
 }
 
 class _LayoutState extends State<Layout> {
-
   Position firstOya = Position.Bottom;
   final Color firstOyaColor = Colors.red;
   final Color nonFirstOyaColor = Colors.black;
-  final List<String> kyokus = ["東一局", "東二局", "東三局", "東四局", "南一局", "南二局", "南三局", "南四局", "西一局", "西二局", "西三局", "西四局", "北一局", "北二局", "北三局", "北四局"];
   SettingParameter currentSetting;
   PointSettingParameter currentPointSetting;
 
   @override
   void initState() {
     super.initState();
-    currentSetting = SettingParameter(startingPoint: 30000, givenStartingPoint: 25000, riichibouPoint: 1000, bonbaPoint: 300, umaBig: 20, umaSmall: 10, kiriage: false, douten: false);
+    currentSetting = SettingParameter(startingPoint: 30000, givenStartingPoint: 25000, riichibouPoint: 1000, bonbaPoint: 300, ryukyokuPoint: 3000, umaBig: 20, umaSmall: 10, kiriage: false, douten: false);
     Map<Position, Player> players = Map();
     players[Position.Bottom] = Player(position: Position.Bottom, point: currentSetting.givenStartingPoint);
     players[Position.Right] = Player(position: Position.Right, point: currentSetting.givenStartingPoint);
@@ -57,6 +58,59 @@ class _LayoutState extends State<Layout> {
     void savePointSetting(PointSettingParameter pointSetting) {
       setState(() {
         currentPointSetting = pointSetting;
+      });
+    }
+
+    void updatePlayerPointTsumo(int point, Position position) {
+      Position oya = Position.values[(firstOya.index + currentPointSetting.currentKyoku) % 4];
+      if (position == oya) {
+        point *= 2;
+      }
+      currentPointSetting.players.forEach((key, value) {
+        if (key == position) {
+          if (key == oya) {
+            value.point += (point + 99) ~/ 100 * 100 * 3;
+          } else {
+            value.point += (point + 99) ~/ 100 * 100 * 2 + (point * 2 + 99) ~/ 100 * 100;
+          }
+        } else {
+          if (key == oya) {
+            value.point -= (point * 2 + 99) ~/ 100 * 100;
+          } else {
+            value.point -= (point + 99) ~/ 100 * 100;
+          }
+        }
+      });
+    }
+
+    void calRyukyoku(Map<Position, bool> tenpai, Map<Position, bool> nagashimangan) {
+      int numOfTenpai = 0;
+      int numOfNagashimagan = 0;
+      tenpai.forEach((key, value) {if (value) numOfTenpai++;});
+      nagashimangan.forEach((key, value) {if (value) numOfNagashimagan++;});
+      Position oya = Position.values[(firstOya.index + currentPointSetting.currentKyoku) % 4];
+      setState(() {
+        if (numOfNagashimagan > 0) {
+          nagashimangan.forEach((key, value) {
+            if (value) {
+              updatePlayerPointTsumo(2000, key);
+            }
+          });
+        } else {
+          if (numOfTenpai != 0 && numOfTenpai != 4) {
+            tenpai.forEach((key, value) {
+              if (value) {
+                currentPointSetting.players[key].point += currentSetting.ryukyokuPoint ~/ numOfTenpai;
+              } else {
+                currentPointSetting.players[key].point -= currentSetting.ryukyokuPoint ~/ (4 - numOfTenpai);
+              }
+            });
+          }
+        }
+        if (!tenpai[oya]) {
+          currentPointSetting.currentKyoku++;
+        }
+        currentPointSetting.bonba++;
       });
     }
 
@@ -121,7 +175,7 @@ class _LayoutState extends State<Layout> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text(kyokus[currentPointSetting.currentKyoku]),
+                  Text(constant.kyokus[currentPointSetting.currentKyoku]),
                   Text("${currentPointSetting.bonba} 本場"),
                   Text("供托: ${currentPointSetting.riichibou}"),
                 ],
@@ -151,67 +205,15 @@ class _LayoutState extends State<Layout> {
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            RaisedButton(
-              child: Text(
-                "銃和",
-              ),
-              // TODO: fill function after finish page
-              onPressed: null,
-              //textColor: CustomColor.BROWN_DARK,
-              color: Colors.white,
-              elevation: 0.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-            ),
-            RaisedButton(
-              child: Text(
-                "自摸",
-              ),
-              // TODO: fill function after finish page
-              onPressed: null,
-              //textColor: CustomColor.BROWN_DARK,
-              color: Colors.white,
-              elevation: 0.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-            ),
-            RaisedButton(
-              child: Text(
-                "流局",
-              ),
-              // TODO: fill function after finish page
-              onPressed: null,
-              //textColor: CustomColor.BROWN_DARK,
-              color: Colors.white,
-              elevation: 0.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-            ),
-            RaisedButton(
-              child: Text(
-                "重置",
-              ),
-              // TODO: fill function after finish page
-              onPressed: null,
-              //textColor: CustomColor.BROWN_DARK,
-              color: Colors.white,
-              elevation: 0.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-            ),
+            // TODO: fill function after finish page
+            BaseBarButton("銃和", null),
+            BaseBarButton("自摸", null),
+            BaseBarButton("流局", () {Navigator.push(context, MaterialPageRoute(builder: (context) => Ryukyoku(save: calRyukyoku)));}),
+            BaseBarButton("重置", reset),
           ],
         ),
         color: Colors.blue,
---
+++ lib/ryukyoku.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'common.dart';
+
+class Ryukyoku extends StatefulWidget {
+  Function save;
+  Ryukyoku({
+      @required this.save,
+  });
+
+  @override
+  State<Ryukyoku> createState() => _RyokyokuState();
+}
+
+class _RyokyokuState extends State<Ryukyoku> {
+  Map<Position, bool> _tenpai, _nagashimangan;
+
+  @override
+  void initState() {
+    super.initState();
+    _tenpai = Map();
+    _tenpai[Position.Bottom] = false;
+    _tenpai[Position.Right] = false;
+    _tenpai[Position.Top] = false;
+    _tenpai[Position.Left] = false;
+    _nagashimangan = Map();
+    _nagashimangan[Position.Bottom] = false;
+    _nagashimangan[Position.Right] = false;
+    _nagashimangan[Position.Top] = false;
+    _nagashimangan[Position.Left] = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget TenpaiCheckboxListTile(Position position) {
+      return FlexibleCustomCheckBoxTile(
+        _tenpai[position],
+        constant.positionText[position],
+        (val) {
+          setState(() {
+            _tenpai[position] = val;
+          });
+        },
+      );
+    }
+
+    Widget NagashimanganCheckboxListTile(Position position) {
+      return FlexibleCustomCheckBoxTile(
+        _nagashimangan[position],
+        constant.positionText[position],
+            (val) {
+          setState(() {
+            _nagashimangan[position] = val;
+          });
+        },
+      );
+    }
+
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("流局"),
+          automaticallyImplyLeading: false,
+        ),
+        body: Center(
+          child: ListView(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
+            ),
+            shrinkWrap: true,
+            children: [
+              Text("聴牌"),
+              Row(
+                children: [
+                  TenpaiCheckboxListTile(Position.Bottom),
+                  TenpaiCheckboxListTile(Position.Right),
+                ],
+              ),
+              Row(
+                children: [
+                  TenpaiCheckboxListTile(Position.Top),
+                  TenpaiCheckboxListTile(Position.Left),
+                ],
+              ),
+              Text("流局滿貫"),
+              Row(
+                children: [
+                  NagashimanganCheckboxListTile(Position.Bottom),
+                  NagashimanganCheckboxListTile(Position.Right),
+                ],
+              ),
+              Row(
+                children: [
+                  NagashimanganCheckboxListTile(Position.Top),
+                  NagashimanganCheckboxListTile(Position.Left),
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              BaseBarButton("取消", () => Navigator.pop(context)),
+              BaseBarButton("儲存", () {
+                widget.save(_tenpai, _nagashimangan);
+                Navigator.pop(context);
+              }),
+            ],
+          ),
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
