6b1ddbf84ef45d4ec040d382bb0af196ff6c6562
2013612 <samuelwong2013612@gmail.com>
2021-03-14 22:31:07 +0800

    change main.dart according to linter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:jmpr_flutter/locale.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'layout.dart';
+import 'locale.dart';
 
 void main() {
   runApp(JMPRAPP());
