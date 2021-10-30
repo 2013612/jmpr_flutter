8df553ca4a6ff03daa3d47ff1b5deeb480daf2fd
2013612 <samuelwong2013612@gmail.com>
2021-10-30 23:43:21 +0800

    change implementation to i18n

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'pages/main_page/main_page.dart';
+import 'utility/constant.dart';
 import 'utility/providers.dart';
 import 'utility/validators.dart';
 
@@ -14,6 +15,7 @@ class JMPRAPP extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     Validators.context = context;
+    Constant.context = context;
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
