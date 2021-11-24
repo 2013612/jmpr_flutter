5d339a3bacc31ed489da8b17861043e1f2660d51
2013612 <samuelwong2013612@gmail.com>
2021-11-25 03:55:06 +0800

    remove duplicate code and fix bug in showResult

--- lib/pages/main_page/local_widgets/main_landscape.dart
+++ lib/pages/main_page/local_widgets/main_landscape.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../../classes/history.dart';
-import '../../../classes/point_setting.dart';
-import '../../../classes/setting.dart';
 import '../../../providers/histories.dart';
 import '../../../utility/constant.dart';
 import 'point_riichi_display.dart';
@@ -16,103 +13,13 @@ class MainLandscape extends ConsumerWidget {
     final histories = ref.watch(historiesProvider);
     final index = ref.watch(historyIndexProvider);
     final pointSetting = histories[index].pointSetting;
-    final Color _textColor = Colors.white;
-
-    Map<Position, double> calResult(History history) {
-      final PointSetting _pointSetting = history.pointSetting;
-      final Setting _setting = history.setting;
-      List<List<int>> cal = [];
-      Position position(int index) {
-        return Position.values[(_setting.firstOya.index + cal[index][1]) % 4];
-      }
-
-      for (Position position in Position.values) {
-        cal.add([
-          _pointSetting.players[position]!.point - _setting.startingPoint,
-          (Position.values.indexOf(position) -
-                  Position.values.indexOf(_setting.firstOya) +
-                  4) %
-              4,
-        ]);
-      }
-
-      cal.sort((List<int> a, List<int> b) {
-        if (a[0] == b[0]) {
-          return a[1] - b[1];
-        } else {
-          return b[0] - a[0];
-        }
-      });
-
-      Map<Position, double> marks = {};
-      double topBonus =
-          4 * (_setting.startingPoint - _setting.givenStartingPoint) / 1000;
-      if (_setting.isDouten) {
-        if (cal[0][0] == cal[1][0] &&
-            cal[1][0] == cal[2][0] &&
-            cal[2][0] == cal[3][0]) {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus / 4;
-          marks[position(1)] = cal[1][0] / 1000 + topBonus / 4;
-          marks[position(2)] = cal[2][0] / 1000 + topBonus / 4;
-          marks[position(3)] = cal[3][0] / 1000 + topBonus / 4;
-        } else if (cal[0][0] == cal[1][0] && cal[1][0] == cal[2][0]) {
-          marks[position(0)] =
-              cal[0][0] / 1000 + (topBonus + _setting.umaBig) / 3;
-          marks[position(1)] =
-              cal[1][0] / 1000 + (topBonus + _setting.umaBig) / 3;
-          marks[position(2)] =
-              cal[2][0] / 1000 + (topBonus + _setting.umaBig) / 3;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-        } else if (cal[1][0] == cal[2][0] && cal[2][0] == cal[3][0]) {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-          marks[position(1)] = cal[1][0] / 1000 - _setting.umaBig / 3;
-          marks[position(2)] = cal[2][0] / 1000 - _setting.umaBig / 3;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig / 3;
-        } else if (cal[0][0] == cal[1][0] && cal[2][0] == cal[3][0]) {
-          marks[position(0)] = cal[0][0] / 1000 +
-              (topBonus + _setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(1)] = cal[1][0] / 1000 +
-              (topBonus + _setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(2)] =
-              cal[2][0] / 1000 - (_setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(3)] =
-              cal[3][0] / 1000 - (_setting.umaBig + _setting.umaSmall) / 2;
-        } else if (cal[0][0] == cal[1][0]) {
-          marks[position(0)] = cal[0][0] / 1000 +
-              (topBonus + _setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(1)] = cal[1][0] / 1000 +
-              (topBonus + _setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(2)] = cal[2][0] / 1000 - _setting.umaSmall;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-        } else if (cal[1][0] == cal[2][0]) {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-          marks[position(1)] = cal[1][0] / 1000;
-          marks[position(2)] = cal[2][0] / 1000;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-        } else if (cal[2][0] == cal[3][0]) {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-          marks[position(1)] = cal[1][0] / 1000 + _setting.umaSmall;
-          marks[position(2)] =
-              cal[2][0] / 1000 - (_setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(3)] =
-              cal[3][0] / 1000 - (_setting.umaBig + _setting.umaSmall) / 2;
-        } else {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-          marks[position(1)] = cal[1][0] / 1000 + _setting.umaSmall;
-          marks[position(2)] = cal[2][0] / 1000 - _setting.umaSmall;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-        }
-      } else {
-        marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-        marks[position(1)] = cal[1][0] / 1000 + _setting.umaSmall;
-        marks[position(2)] = cal[2][0] / 1000 - _setting.umaSmall;
-        marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-      }
-      return marks;
-    }
+    final middleTextStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 20.0,
+    );
 
     void showResult() {
-      Map<Position, double> marks = calResult(histories.last);
+      Map<Position, double> marks = histories[index].calResult();
 
       showDialog(
         context: context,
@@ -180,26 +87,17 @@ class MainLandscape extends ConsumerWidget {
               ),
               Text(
                 Constant.kyokus[pointSetting.currentKyoku],
-                style: TextStyle(
-                  color: _textColor,
-                  fontSize: 20.0,
-                ),
+                style: middleTextStyle,
               ),
               Spacer(),
               Text(
                 "${pointSetting.bonba} 本場",
-                style: TextStyle(
-                  color: _textColor,
-                  fontSize: 20.0,
-                ),
+                style: middleTextStyle,
               ),
               Spacer(),
               Text(
                 "供托: ${pointSetting.riichibou}",
-                style: TextStyle(
-                  color: _textColor,
-                  fontSize: 20.0,
-                ),
+                style: middleTextStyle,
               ),
               Spacer(
                 flex: 3,
--- lib/pages/main_page/local_widgets/main_portrait.dart
+++ lib/pages/main_page/local_widgets/main_portrait.dart
@@ -3,9 +3,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nil/nil.dart';
 
-import '../../../classes/history.dart';
-import '../../../classes/point_setting.dart';
-import '../../../classes/setting.dart';
 import '../../../providers/histories.dart';
 import '../../../utility/constant.dart';
 import 'point_riichi_display.dart';
@@ -16,103 +13,14 @@ class MainPortrait extends ConsumerWidget {
     final i18n = AppLocalizations.of(context)!;
     final histories = ref.watch(historiesProvider);
     final index = ref.watch(historyIndexProvider);
-    final Color _textColor = Colors.white;
-
-    Map<Position, double> calResult(History history) {
-      final PointSetting _pointSetting = history.pointSetting;
-      final Setting _setting = history.setting;
-      List<List<int>> cal = [];
-      Position position(int index) {
-        return Position.values[(_setting.firstOya.index + cal[index][1]) % 4];
-      }
-
-      for (Position position in Position.values) {
-        cal.add([
-          _pointSetting.players[position]!.point - _setting.startingPoint,
-          (Position.values.indexOf(position) -
-                  Position.values.indexOf(_setting.firstOya) +
-                  4) %
-              4,
-        ]);
-      }
-
-      cal.sort((List<int> a, List<int> b) {
-        if (a[0] == b[0]) {
-          return a[1] - b[1];
-        } else {
-          return b[0] - a[0];
-        }
-      });
-
-      Map<Position, double> marks = {};
-      double topBonus =
-          4 * (_setting.startingPoint - _setting.givenStartingPoint) / 1000;
-      if (_setting.isDouten) {
-        if (cal[0][0] == cal[1][0] &&
-            cal[1][0] == cal[2][0] &&
-            cal[2][0] == cal[3][0]) {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus / 4;
-          marks[position(1)] = cal[1][0] / 1000 + topBonus / 4;
-          marks[position(2)] = cal[2][0] / 1000 + topBonus / 4;
-          marks[position(3)] = cal[3][0] / 1000 + topBonus / 4;
-        } else if (cal[0][0] == cal[1][0] && cal[1][0] == cal[2][0]) {
-          marks[position(0)] =
-              cal[0][0] / 1000 + (topBonus + _setting.umaBig) / 3;
-          marks[position(1)] =
-              cal[1][0] / 1000 + (topBonus + _setting.umaBig) / 3;
-          marks[position(2)] =
-              cal[2][0] / 1000 + (topBonus + _setting.umaBig) / 3;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-        } else if (cal[1][0] == cal[2][0] && cal[2][0] == cal[3][0]) {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-          marks[position(1)] = cal[1][0] / 1000 - _setting.umaBig / 3;
-          marks[position(2)] = cal[2][0] / 1000 - _setting.umaBig / 3;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig / 3;
-        } else if (cal[0][0] == cal[1][0] && cal[2][0] == cal[3][0]) {
-          marks[position(0)] = cal[0][0] / 1000 +
-              (topBonus + _setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(1)] = cal[1][0] / 1000 +
-              (topBonus + _setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(2)] =
-              cal[2][0] / 1000 - (_setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(3)] =
-              cal[3][0] / 1000 - (_setting.umaBig + _setting.umaSmall) / 2;
-        } else if (cal[0][0] == cal[1][0]) {
-          marks[position(0)] = cal[0][0] / 1000 +
-              (topBonus + _setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(1)] = cal[1][0] / 1000 +
-              (topBonus + _setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(2)] = cal[2][0] / 1000 - _setting.umaSmall;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-        } else if (cal[1][0] == cal[2][0]) {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-          marks[position(1)] = cal[1][0] / 1000;
-          marks[position(2)] = cal[2][0] / 1000;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-        } else if (cal[2][0] == cal[3][0]) {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-          marks[position(1)] = cal[1][0] / 1000 + _setting.umaSmall;
-          marks[position(2)] =
-              cal[2][0] / 1000 - (_setting.umaBig + _setting.umaSmall) / 2;
-          marks[position(3)] =
-              cal[3][0] / 1000 - (_setting.umaBig + _setting.umaSmall) / 2;
-        } else {
-          marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-          marks[position(1)] = cal[1][0] / 1000 + _setting.umaSmall;
-          marks[position(2)] = cal[2][0] / 1000 - _setting.umaSmall;
-          marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-        }
-      } else {
-        marks[position(0)] = cal[0][0] / 1000 + topBonus + _setting.umaBig;
-        marks[position(1)] = cal[1][0] / 1000 + _setting.umaSmall;
-        marks[position(2)] = cal[2][0] / 1000 - _setting.umaSmall;
-        marks[position(3)] = cal[3][0] / 1000 - _setting.umaBig;
-      }
-      return marks;
-    }
+    final pointSetting = histories[index].pointSetting;
+    final middleTextStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 20.0,
+    );
 
     void showResult() {
-      Map<Position, double> marks = calResult(histories.last);
+      Map<Position, double> marks = histories[index].calResult();
 
       showDialog(
         context: context,
@@ -164,25 +72,16 @@ class MainPortrait extends ConsumerWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Text(
-                  Constant.kyokus[histories[index].pointSetting.currentKyoku],
-                  style: TextStyle(
-                    color: _textColor,
-                    fontSize: 20.0,
-                  ),
+                  Constant.kyokus[pointSetting.currentKyoku],
+                  style: middleTextStyle,
                 ),
                 Text(
-                  "${histories[index].pointSetting.bonba} 本場",
-                  style: TextStyle(
-                    color: _textColor,
-                    fontSize: 20.0,
-                  ),
+                  "${pointSetting.bonba} 本場",
+                  style: middleTextStyle,
                 ),
                 Text(
-                  "供托: ${histories[index].pointSetting.riichibou}",
-                  style: TextStyle(
-                    color: _textColor,
-                    fontSize: 20.0,
-                  ),
+                  "供托: ${pointSetting.riichibou}",
+                  style: middleTextStyle,
                 ),
               ],
             ),
