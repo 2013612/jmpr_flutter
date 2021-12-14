39d64bf65f926afd1fa60cdb95b8b0beae629c3b
2013612 <samuelwong2013612@gmail.com>
2021-12-15 02:39:53 +0800

    add UserRepository for communicate with firestore

---
+++ lib/classes/user_repository.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'user.dart';
+
+class UserRepository {
+  final CollectionReference<User> collection = FirebaseFirestore.instance
+      .collection('users')
+      .withConverter<User>(
+          fromFirestore: (snapshot, _) => User.fromSnapshot(snapshot),
+          toFirestore: (user, _) => user.toJson());
+
+  Stream<QuerySnapshot<User>> getUserStream(String uid) {
+    return collection.where("uid", isEqualTo: uid).snapshots();
+  }
+
+  Stream<QuerySnapshot<User>> listUsersStream() {
+    return collection.snapshots();
+  }
+
+  Future<QuerySnapshot<User>> getUserByName(String displayName) {
+    return collection.where("display_name", isEqualTo: displayName).get();
+  }
+
+  Future<DocumentReference> addUser(User user) async {
+    final existUser = await getUserByName(user.displayName ?? "");
+    if (existUser.docs.isNotEmpty) {
+      user.displayName = "${user.displayName!}#2";
+    }
+    return collection.add(user);
+  }
+}
