e0349bdbc0dda1356d04a200f7fd8bca47290cf5
2013612 <samuelwong2013612@gmail.com>
2021-12-20 23:28:00 +0800

    add JsonValue for enum

--- lib/utility/enum/ending.dart
+++ lib/utility/enum/ending.dart
@@ -1,7 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
 enum Ending {
+  @JsonValue('START')
   start,
+  @JsonValue('EDIT')
   edit,
+  @JsonValue('RON')
   ron,
+  @JsonValue('TSUMO')
   tsumo,
+  @JsonValue('RYUKYOKU')
   ryukyoku,
 }
--- lib/utility/enum/position.dart
+++ lib/utility/enum/position.dart
@@ -1,6 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
 enum Position {
+  @JsonValue('BOTTOM')
   bottom,
+  @JsonValue('RIGHT')
   right,
+  @JsonValue('TOP')
   top,
+  @JsonValue('LEFT')
   left,
 }
