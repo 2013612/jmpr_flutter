2da350cf81fe6a36302b9ce94d018c341a131bc0
2013612 <samuelwong2013612@gmail.com>
2021-12-20 23:39:50 +0800

    use models

--- lib/providers/user_stream.dart
+++ lib/providers/user_stream.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../classes/user.dart';
 import '../classes/user_repository.dart';
+import '../models/user.dart';
 
 final userStreamProvider =
     StreamProvider.autoDispose.family<QuerySnapshot<User>, String>((_, uid) {
